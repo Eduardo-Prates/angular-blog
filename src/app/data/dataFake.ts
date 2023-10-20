fdc9916d40a03bc950d771e34fe60f42ee9254c8
Eduardo Prates Tiadoro <eduardotiadoro@gmail.com>
2023-10-20 16:00:50 -0300

    Dynamic content attribution

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://sm.ign.com/t/ign_br/tv/o/one-piece-/one-piece-2_1xby.1200.jpg",
+    "title": "One Piece: A Verdadeira História do Mundo",
+    "description": "Essa teoria final para o One Piece imagina uma narrativa que enfatiza a importância do conhecimento e da busca da verdade, enquanto desafia o status quo opressivo do mundo. É importante lembrar que esta é apenas uma especulação e a história real pode ser muito diferente, mas é divertido imaginar como a história de One Piece poderia se desenrolar."
+  },
+  {
+    "id": "2",
+    "photo": "https://preview.redd.it/c9brn2stvwqb1.jpg?auto=webp&s=e657f412452a9ab7b3d73cb5b1d6fc7ecbaa3c2c",
+    "title": "Happy Birthday One Piece! Celebrating 23 Years of the Best Manga Ever",
+    "description": "Eiichiro Oda's One Piece manga is celebrating its 23rd anniversary this year, and it's still going strong. The manga has sold over 430 million copies worldwide, making it the best-selling manga series in history. It's also the best-selling comic series of all time, and the third best-selling book series in history, behind only Harry Potter and Goosebumps."
+  },
+  {
+    "id": "3",
+    "photo": "https://i.ytimg.com/vi/AGwYfGRhBco/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLALzSyaPFuk1M3JgIuvYH3SfYB0kg",
+    "title": "One Piece Red (Straw Hat Pirates: Pirate Popularity Poll)",
+    "description": "One Piece Red: Grand Characters is the fifth databook. It was released on March 2, 2002 and coincided with the release of the One Piece Color Walk 2. It contains information on various characters, story arcs, and the SBS."
+  },
+  {
+    "id": "4",
+    "photo": "https://static1.cbrimages.com/wordpress/wp-content/uploads/2019/09/10-Hilarious-One-Piece-Memes-Only-True-Fans-Will-Understand-featured-image.jpg",
+    "title": "10 Hilarious One Piece Memes Only True Fans Will Understand",
+    "description": "One Piece is one of the longest-running anime series in the world, and as such, it's developed a lot of memes. Here are the best of the best."
+  }
+]
